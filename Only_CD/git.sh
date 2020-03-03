d810ce4ecbc534d4c696a82ecc5bee161f259513
kalikichandu <kaliki.chandrasekhar@tcs.com>
2020-03-04 01:05:55 +0530

    Adding Git File

---
+++ Only_CD/git.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/kalikichandu/ML-Model-Retraining-DevOps-way.git
+cd ML-Model-Retraining-DevOps-way
+touch sample.txt
+git add .
+git commit -m'Adding Sample File'
+git push
